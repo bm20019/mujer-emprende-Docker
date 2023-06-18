f62186b49d7151887f56fbd81de1fbfa3b6d11a4
bm20019 <bm20019@ues.edu.sv>
2023-06-17 22:58:55 -0600

    Modificando bash de ip cambio

--- FRONT/emprende/script.sh
+++ FRONT/emprende/script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pwd
 # Obtener la dirección IP pública del host
-ip=$(curl -s http://checkip.amazonaws.com)
-
+# ip=$(curl -s http://checkip.amazonaws.com)
+ipR=$(ip addr show eth1 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
+domain="ip${ipR}-${SESSION_ID}-8080.${PWD_HOST_FQDN}"
 # Reemplazar la cadena de texto en el archivo
-sed -i "s/localhost/$ip/g" src/app/components/principal/principal.component.ts
+sed -i "s/localhost/$domain/g" src/app/components/principal/principal.component.ts
 
 echo "La dirección IP pública del host es: $ip"
