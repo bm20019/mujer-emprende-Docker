42ee569e9f464eb3d4bd6ca90a93b1aee97a07a2
bm20019 <111785818+bm20019@users.noreply.github.com>
2023-06-17 23:53:08 -0600

    modificar {HOST} por direct.labs.play-with-docker.com

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ pwd
 # Obtener la dirección IP pública del host
 # ip=$(curl -s http://checkip.amazonaws.com)
 ipR=$(ip addr show eth1 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
-domain="ip${ipR}-${SESSION_ID}-8080.${PWD_HOST_FQDN}"
+domain="ip${ipR}-${SESSION_ID}-8080.direct.labs.play-with-docker.com"
 # Reemplazar la cadena de texto en el archivo
 sed -i "s/localhost/$domain/g" FRONT/emprende/src/app/components/principal/principal.component.ts
 
