f7bece5de00a37d6aeb2abc1459d34488ed29aa6
bm20019 <bm20019@ues.edu.sv>
2023-06-17 23:04:45 -0600

    Modificando script ruta de pricipal

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@ pwd
 ipR=$(ip addr show eth1 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
 domain="ip${ipR}-${SESSION_ID}-8080.${PWD_HOST_FQDN}"
 # Reemplazar la cadena de texto en el archivo
-sed -i "s/localhost/$domain/g" src/app/components/principal/principal.component.ts
+sed -i "s/localhost/$domain/g" FRONT/emprende/src/app/components/principal/principal.component.ts
 
 echo "La dirección IP pública del host es: $domain"
