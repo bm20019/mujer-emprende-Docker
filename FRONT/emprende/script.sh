e39554cf89460416afde594a99069311c39ecdce
bm20019 <bm20019@ues.edu.sv>
2023-06-17 23:00:07 -0600

    solucionado error script

--- FRONT/emprende/script.sh
+++ FRONT/emprende/script.sh
@@ -7,4 +7,4 @@ domain="ip${ipR}-${SESSION_ID}-8080.${PWD_HOST_FQDN}"
 # Reemplazar la cadena de texto en el archivo
 sed -i "s/localhost/$domain/g" src/app/components/principal/principal.component.ts
 
-echo "La dirección IP pública del host es: $ip"
+echo "La dirección IP pública del host es: $domain"
